ca0fdb995871fb93f700bd7eda9cf9a004f68a39
Quyk Mendonça <osilmar@outlook.com>
2019-10-20 00:40:39 -0300

    Adiciona Endpoint de Áreas de produção

---
+++ src/StoreCatalog.Api/Controllers/ProductionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreCatalog.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StoreCatalog.Api.Controllers
+{
+    [Route("api/    ")]
+    public class ProductionController : Controller
+    {
+        [HttpGet("areas")]
+        public async Task<ActionResult<AreasModel>> GetAreas()
+        {
+            var area = new AreasModel
+            {
+                ProductionId = new Guid("28AAE9F5-F9F2-499F-AB2A-134EAF42CB19"),
+                Restrictions = new List<string>
+                {
+                    "soy",
+                    "dairy",
+                    "gluten",
+                    "peanut"
+                },
+                On = true
+            };
+            return new OkObjectResult(area);
+        }
+    }
+}
---
+++ src/StoreCatalog.Api/Models/AreasModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreCatalog.Api.Models
+{
+    public class AreasModel
+    {
+        public Guid ProductionId { get; set; }
+        public IList<string> Restrictions { get; set; }
+        public bool On { get; set; }
+    }
+}
