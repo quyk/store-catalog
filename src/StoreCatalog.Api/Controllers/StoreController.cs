460c2dfbe29cd9d99703a47624caf9f147814cf8
Quyk Mendonça <osilmar@outlook.com>
2019-10-20 01:03:28 -0300

    Adiciona Endpoint de Store

---
+++ src/StoreCatalog.Api/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreCatalog.Api.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace StoreCatalog.Api.Controllers
+{
+    [Route("api/store")]
+    public class StoreController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<StoreModel>> GetStore()
+        {
+            var store = new StoreModel
+            {
+                StoreId = new Guid("FC7DE87A-741F-4558-93C9-3A14CC3B22E8"),
+                Ready = true
+            };
+
+            return new OkObjectResult(store);
+        }
+    }
+}
---
+++ src/StoreCatalog.Api/Models/StoreModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StoreCatalog.Api.Models
+{
+    public class StoreModel
+    {
+        public Guid StoreId { get; set; }
+        public bool Ready { get; set; }
+    }
+}
