653f1c8193f666d959de27bc44e75e8302e9b5d2
Quyk Mendonça <osilmar@outlook.com>
2019-10-20 00:39:33 -0300

    Adiciona Endpoint de Produtos

---
+++ src/StoreCatalog.Api/Models/ItemModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StoreCatalog.Api.Models
+{
+    public class ItemModel
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+}
